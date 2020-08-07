e387285a6fa578a8cd2d2ed9974eec6d1e82da0e
Daan <daanrosendal@outlook.com>
2020-08-07 11:21:02 +0200

    switched to a different cat api

--- fun-stuff/random-cats/script.js
+++ fun-stuff/random-cats/script.js
@@ -7,15 +7,18 @@ function getRandomCatPicture() {
     $('#newCatButton').addClass('is-loading');
 
     $.ajax({
-        url:"https://aws.random.cat/meow", //the page containing php script
-        type: "post", //request type,
+        url:"https://api.thecatapi.com/v1/images/search", //the page containing php script
+        type: "get", //request type,
         dataType: 'json',
         success: function(result){
-            $('#catPicture').attr('src', result.file);
+            $('#catPicture').attr('src', result[0].url);
         }
     });
 }
 
 function removeIsLoading(){
     $('#newCatButton').removeClass('is-loading');
+
+    // To remove the unfixable warning message having to do with cross site cookies
+    console.clear();
 }
